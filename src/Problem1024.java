b288aeed9a483388dbfa16ff1b02e4125612f591
edevyatkin <edevyatkin@debian>
2015-05-27 19:15:51 +0300

    problem 1024 is solved

--- src/Problem1024.java
+++ src/Problem1024.java
@@ -1,35 +1,33 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-// wip
-
 public class Problem1024 {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		int[] list1 = new int[sc.nextInt()];
-		for (int i = 0; i < list1.length; i++) {
+		for (int i = 0; i < list1.length; i++)
 			list1[i] = sc.nextInt();
-		}
-		
 		sc.close();
 		
-		int[] list2 = new int[list1.length];
-		
-		int order = getPertumationOrder(list1, list2, 1);
+		int order = getPertumationOrder(list1, list1);
 		System.out.println(order);
-		
 	}
 
-	private static int getPertumationOrder(int[] list1, int[] list2, int order) {
-		if (Arrays.equals(list1, list2))
-			return order;
-		for (int i = 0; i < list1.length; i++) {
-			list2[i] = list1[list1[i]-1];
+	private static int getPertumationOrder(int[] list1, int[] list2) {
+		if (Arrays.equals(list1, list2) && list1 != list2)
+			return 0;
+		
+		int[] list3 = new int[list1.length];
+		for (int i = 0; i < list2.length; i++) {
+			int index = list2[i] - 1;
+			list3[i] = list1[index];
 		}
-		System.out.println(Arrays.toString(list2));
-		return order+getPertumationOrder(list2, list1, order+1);
+		
+		list2 = null;
+		
+		return 1 + getPertumationOrder(list1, list3);
 	}
 
 }
